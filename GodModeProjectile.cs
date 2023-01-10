9776a05cd7b8230d5e37cc20f7ff1c6355671ce9
First Fractal <firstfractal512@gmail.com>
2023-01-10 15:44:38 -0600

    made the god mode icon appear above players who are in god mode

---
+++ GodModeProjectile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace JMGMN
+{
+    internal class GodModeProjectile : ModProjectile
+    {
+        //set the sprite for the projectile
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 1;
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            //set the stats for it
+            Projectile.netImportant = true;
+            Projectile.friendly = true;
+            Projectile.aiStyle = -1;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.timeLeft = 9999999;
+        }
+
+        public override void AI()
+        {
+            //check if the player not is dead and is in godmode. if so, the constanly move and hover. if not, then kill it.
+            Projectile.timeLeft = 9999999;
+            Player player = Main.player[Projectile.owner];
+            if (player.creativeGodMode && !player.dead)
+            {
+                Projectile.position = player.position + new Vector2(-2.5f, -60);
+            }
+            else
+            {
+                Projectile.Kill();
+            }
+        }
+    }
+}
---
+++ ModdedPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace JMGMN
+{
+    class ModdedPlayer : ModPlayer
+    {
+        public bool SpawnedInGod = false;
+        public override void PostUpdate()
+        {
+            //check if the player is in godmode, and spawn in the icon when in godmode
+            if (Player.creativeGodMode)
+            {
+                if (SpawnedInGod == false)
+                {
+                    Projectile.NewProjectile(Player.GetSource_FromThis(), Player.position, Vector2.Zero, ModContent.ProjectileType<GodModeProjectile>(), 0, 0f, Player.whoAmI);
+                    SpawnedInGod = true;
+                }
+            } else
+            {
+                SpawnedInGod = false;
+            }
+            base.PostUpdate();
+        }
+    }
+}
